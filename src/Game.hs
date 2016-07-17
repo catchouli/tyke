067182f139394a35bc11dda0e57a07557e9ffeac
Caitlin Wilks <cat@dev.learny.click>
2016-07-17 22:31:01 +0100

    added some basic crappy keyboard input and a moving "player"

--- src/Game.hs
+++ src/Game.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Game
   ( gameNetwork
   )
@@ -7,11 +9,14 @@ import Reactive.Banana
 import Reactive.Banana.Frameworks
 
 import qualified SDL.Event                       as SDL
+import qualified SDL.Input.Keyboard              as SDL
+import qualified SDL.Input.Keyboard.Codes        as SDL
 import qualified Graphics.Gloss                  as Gloss
 import qualified Graphics.Gloss.Rendering        as Gloss
 
 
 data Game = Game { counter :: Int
+                 , pos :: (Int, Int)
                  }
 
 -- Function to render a gloss picture
@@ -22,9 +27,29 @@ renderGame glossState = Gloss.displayPicture (800, 600) Gloss.black glossState 1
 -- I wish I could avoid passing this gloss state
 gameNetwork :: Gloss.State -> Event (SDL.Event) -> Event () -> MomentIO (Behavior (IO ()))
 gameNetwork glossState eInput eUpdate = do
-  bCounter <- accumB 0 $ unions [ (+1) <$ eInput ]
-  let bGame = Game <$> bCounter
+  let filterKeyEvent e = (\case SDL.KeyboardEvent _ -> True; _ -> False) $ SDL.eventPayload e
+  let filterKey scancode (SDL.Event _ (SDL.KeyboardEvent (SDL.KeyboardEventData _ _ _ (SDL.Keysym eScan _ _)))) = eScan == scancode
 
+  let keyEvents = filterE filterKeyEvent eInput
+  let leftKeyEvent  = filterE (filterKey SDL.ScancodeLeft)  keyEvents
+  let rightKeyEvent = filterE (filterKey SDL.ScancodeRight) keyEvents
+  let downKeyEvent  = filterE (filterKey SDL.ScancodeDown)  keyEvents
+  let upKeyEvent    = filterE (filterKey SDL.ScancodeUp)    keyEvents
+
+  playerX <- accumB (0 :: Int) $ unions [ subtract 9 <$ leftKeyEvent
+                                        , (+9)       <$ rightKeyEvent ]
+  playerY <- accumB (0 :: Int) $ unions [ subtract 9 <$ downKeyEvent
+                                        , (+9)       <$ upKeyEvent ]
+
+  let bPos = (,) <$> playerX <*> playerY
+
+  -- Count input events
+  bCounter <- accumB 0 $ unions [ (+1) <$ keyEvents ]
+
+  -- Compose game behaviour
+  let bGame = Game <$> bCounter <*> bPos
+
+  -- Compose game rendering
   return $ renderGame glossState . render <$> bGame
 
 
@@ -32,6 +57,7 @@ gameNetwork glossState eInput eUpdate = do
 render :: Game -> Gloss.Picture
 render game =
   let count = counter game
-    in Gloss.Pictures [ Gloss.Color Gloss.white $ Gloss.circle 10
+      (playerX, playerY) = pos game
+    in Gloss.Pictures [ Gloss.Color Gloss.white $ Gloss.translate (fromIntegral playerX) (fromIntegral playerY) $ Gloss.circle 10
                       , Gloss.Color Gloss.white $ Gloss.text (show count)
                       ]
