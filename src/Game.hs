e0e81ea46647f766f47a7c6d58b2e7b7c79dbe0b
Caitlin Wilks <cat@dev.learny.click>
2016-07-20 00:03:38 +0100

    split GameState up into some more type aliases and tidy some other stuff up

--- src/Game.hs
+++ src/Game.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-
 module Game
   ( initialGameState
   , update
@@ -13,11 +10,7 @@ import Prelude hiding ((.))
 import Control.Wire hiding (id)
 import FRP.Netwire.Move
 
-import qualified SDL.Event                       as SDL
-import qualified SDL.Input.Keyboard              as SDL
-import qualified SDL.Input.Keyboard.Codes        as SDL
 import qualified Graphics.Gloss                  as Gloss
-import qualified Graphics.Gloss.Rendering        as Gloss
 
 
 data Game = Game { _counter :: Double
@@ -26,14 +19,16 @@ data Game = Game { _counter :: Double
 
 -- A simple counter
 counterWire :: (Monad m, HasTime t s) => Wire s e m a Double
-counterWire = integral 0 . 1
+counterWire = integral 0 . (pure 1)
 
 
 -- The game state type, comprising of a session (fixed timestep), a main game wire, and the last game state
-type GameState x e a = (Session Identity (Timed NominalDiffTime ()), Wire (Timed NominalDiffTime ()) e Identity a Game, Game)
+type GameSession = Timed NominalDiffTime ()
+type GameWire e a = Wire GameSession e Identity a Game
+type GameState x e a = (Session Identity GameSession, GameWire e a, Game)
 
 
--- Function to render a gloss picture
+-- The initial game state
 initialGameState :: GameState x e a
 initialGameState = let session = countSession 1 <*> pure ()
                        wire = Game <$> counterWire
@@ -42,7 +37,6 @@ initialGameState = let session = countSession 1 <*> pure ()
 
 
 -- Update the game
---update :: GameState x e -> GameState x e
 update :: GameState Int e () -> GameState Int e ()
 update (session, wire, _) = let (s, session') = runIdentity $ stepSession session
                                 (r, wire') = runIdentity $ stepWire wire s (Right mempty)
@@ -52,10 +46,8 @@ update (session, wire, _) = let (s, session') = runIdentity $ stepSession sessio
 
 
 -- Render the game to a Gloss.Picture
---render :: GameState x e -> Gloss.Picture
 render (_, _, game) =
-  let 
-      count = _counter game
+  let count = _counter game
       (playerX, playerY) = (0, 0)
     in Gloss.Pictures [ Gloss.Color Gloss.white $ Gloss.translate (fromIntegral playerX) (fromIntegral playerY) $ Gloss.circle 10
                       , Gloss.Color Gloss.white $ Gloss.text (show count)
