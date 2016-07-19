aa95d83657660bc936573a96a847d0e48089a1d4
Caitlin Wilks <cat@dev.learny.click>
2016-07-19 23:55:09 +0100

    finally got netwire working in a pure way..

--- src/Game.hs
+++ src/Game.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Game
   ( initialGameState
@@ -7,6 +8,11 @@ module Game
   )
 where
 
+import Prelude hiding ((.))
+
+import Control.Wire hiding (id)
+import FRP.Netwire.Move
+
 import qualified SDL.Event                       as SDL
 import qualified SDL.Input.Keyboard              as SDL
 import qualified SDL.Input.Keyboard.Codes        as SDL
@@ -14,25 +20,43 @@ import qualified Graphics.Gloss                  as Gloss
 import qualified Graphics.Gloss.Rendering        as Gloss
 
 
-data Game = Game { counter :: Int
-                 , pos :: (Int, Int)
+data Game = Game { _counter :: Double
                  }
 
 
+-- A simple counter
+counterWire :: (Monad m, HasTime t s) => Wire s e m a Double
+counterWire = integral 0 . 1
+
+
+-- The game state type, comprising of a session (fixed timestep), a main game wire, and the last game state
+type GameState x e a = (Session Identity (Timed NominalDiffTime ()), Wire (Timed NominalDiffTime ()) e Identity a Game, Game)
+
+
 -- Function to render a gloss picture
-initialGameState = Game 0 (0, 0)
+initialGameState :: GameState x e a
+initialGameState = let session = countSession 1 <*> pure ()
+                       wire = Game <$> counterWire
+                       game = Game 0.0
+                    in (session, wire, game)
 
 
 -- Update the game
-update :: Game -> Game
-update = id
+--update :: GameState x e -> GameState x e
+update :: GameState Int e () -> GameState Int e ()
+update (session, wire, _) = let (s, session') = runIdentity $ stepSession session
+                                (r, wire') = runIdentity $ stepWire wire s (Right mempty)
+                                -- Irrefutable if the game wire inhibits
+                                Right game' = r
+                            in (session', wire', game')
 
 
 -- Render the game to a Gloss.Picture
-render :: Game -> Gloss.Picture
-render game =
-  let count = counter game
-      (playerX, playerY) = pos game
+--render :: GameState x e -> Gloss.Picture
+render (_, _, game) =
+  let 
+      count = _counter game
+      (playerX, playerY) = (0, 0)
     in Gloss.Pictures [ Gloss.Color Gloss.white $ Gloss.translate (fromIntegral playerX) (fromIntegral playerY) $ Gloss.circle 10
                       , Gloss.Color Gloss.white $ Gloss.text (show count)
                       ]
