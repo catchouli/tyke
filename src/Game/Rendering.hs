f052b9e649b9e84c69782e3cb203f2bc5c36fa58
Caitlin Wilks <cat@dev.learny.click>
2016-07-24 15:28:54 +0100

    fixed matrices and fps camera movement

--- src/Game/Rendering.hs
+++ src/Game/Rendering.hs
@@ -15,21 +15,19 @@ module Game.Rendering
   )
 where
 
-import Game.Data
-import qualified LambdaCube.Linear as LC
-import LambdaCube.GL
-import LambdaCube.GL.Mesh
-import Data.Aeson
-import Codec.Picture
+import Control.Lens
 import System.Random
+import Game.Data
 import Game.Terrain
 import Game.Terrain.Rendering
+import Data.Aeson
 import Data.IORef
-import Data.Coerce
-import qualified Linear.V3                       as Linear
-import qualified Linear.V4                       as Linear
-import qualified Linear.Matrix                   as Linear
+import Linear
 import qualified SDL
+import qualified Codec.Picture                   as Juicy
+import qualified LambdaCube.GL                   as LC
+import qualified LambdaCube.GL.Mesh              as LC
+import qualified LambdaCube.Linear               as LC
 import qualified Data.Map                        as Map
 import qualified Data.Vector                     as V
 import qualified Data.ByteString                 as BS
@@ -41,17 +39,17 @@ renderGame :: IO (Game -> IO ())
 renderGame = do
 
   -- Set up lambdacube
-  let inputSchema = makeSchema $ do
-        defObjectArray "objects" Triangles $ do
-            "position"        @: Attribute_V3F
-            "uv"              @: Attribute_V2F
-        defUniforms $ do
-            "viewMat"         @: M44F
-            "time"            @: Float
-            "diffuseTexture"  @: FTexture2D
+  let inputSchema = LC.makeSchema $ do
+        LC.defObjectArray "objects" LC.Triangles $ do
+            "position"        LC.@: LC.Attribute_V3F
+            "uv"              LC.@: LC.Attribute_V2F
+        LC.defUniforms $ do
+            "viewMat"         LC.@: LC.M44F
+            "time"            LC.@: LC.Float
+            "diffuseTexture"  LC.@: LC.FTexture2D
 
   -- Allocate storage
-  storage <- allocStorage inputSchema
+  storage <- LC.allocStorage inputSchema
 
   -- Upload mesh data
   --uploadMeshToGPU triangleA >>= addMeshToObjectArray storage "objects" []
@@ -59,31 +57,35 @@ renderGame = do
   --uploadMeshToGPU box >>= addMeshToObjectArray storage "objects" []
   
   -- Load texture
-  Right img <- readImage "data/patchouli.png"
-  textureData <- uploadTexture2DToGPU img
+  Right img <- Juicy.readImage "data/patchouli.png"
+  textureData <- LC.uploadTexture2DToGPU img
 
   -- Load pipeline and generate renderer
   Just pipelineDesc <- decodeStrict <$> BS.readFile "data/pipelines/3d.json"
-  renderer <- allocRenderer pipelineDesc
+  renderer <- LC.allocRenderer pipelineDesc
 
   -- Generate some random terrain
   terrain <- randomChunk (15, 15, 15)
   let terrainMesh = genChunkMesh terrain
-  uploadMeshToGPU terrainMesh >>= addMeshToObjectArray storage "objects" []
+  LC.uploadMeshToGPU terrainMesh >>= LC.addMeshToObjectArray storage "objects" []
 
   -- Set storage
-  setStorage renderer storage
+  LC.setStorage renderer storage
 
   -- Get start ticks
   start <- SDL.ticks
 
+  -- An IORef for storing the ticks value
   lastTicksRef <- newIORef start
 
   -- The render handler to return
   return $ \game -> do
-    let Linear.V3 cx cy cz = _camPos game
-    let viewMat = convertMatrix (Linear.mkTransformation (_camRot game) (_camPos game))
+    --let Linear.V3 cx cy cz = _camPos game
+    --let viewMat = convertMatrix (Linear.mkTransformation (_camRot game) (_camPos game))
     --let viewMat = convertMatrix (Linear.mkTransformationMat (Linear.identity) (_camPos game))
+    let mTranslation = identity & translation .~ (-(_camPos game)) :: M44 Float
+    let mRotation = m33_to_m44 . fromQuaternion $ _camRot game :: M44 Float
+    let viewMat = convertMatrix $ (mRotation !*! mTranslation)
 
     lastTicks <- readIORef lastTicksRef
     ticks <- SDL.ticks
@@ -97,82 +99,23 @@ renderGame = do
     let diff = (fromIntegral (ticks - start)) / 100
     let time = (fromIntegral ticks / 2000)
 
-    setScreenSize storage 800 600
-    updateUniforms storage $ do
-      "viewMat" @= return viewMat
-      "diffuseTexture" @= return textureData
-      "time" @= return (time :: Float)
+    LC.setScreenSize storage 800 600
+    LC.updateUniforms storage $ do
+      "viewMat"        LC.@= return viewMat
+      "diffuseTexture" LC.@= return textureData
+      "time"           LC.@= return (time :: Float)
 
-    renderFrame renderer
-
-
--- | Generate a box
-
-box :: Mesh
-box = let faces = [minBound .. maxBound] :: [BoxFace]
-          vertices = concatMap genBoxFace faces
-          uvs = concat . replicate 6 $ [V2 0 0, V2 1 0, V2 0 1, V2 0 1, V2 1 0, V2 1 1]
-      in Mesh
-          { mAttributes   = Map.fromList
-              [ ("position",  A_V3F $ V.fromList vertices )
-              , ("uv",        A_V2F $ V.fromList uvs )
-              ]
-          , mPrimitive = P_Triangles
-          }
-
--- | A data type describing the current face of a box
-
-data BoxFace = PositiveX | NegativeX | PositiveY | NegativeY | PositiveZ | NegativeZ deriving (Show, Enum, Bounded)
-
-
-faceFun :: BoxFace -> V2 Float -> V3 Float
-faceFun PositiveX = \(V2 y z) -> V3 0.5 y z
-faceFun NegativeX = \(V2 y z) -> V3 (-0.5) y (-z)
-faceFun PositiveY = \(V2 x z) -> V3 (-x) 0.5 z
-faceFun NegativeY = \(V2 x z) -> V3 x (-0.5) z
-faceFun PositiveZ = \(V2 x y) -> V3 x y 0.5
-faceFun NegativeZ = \(V2 x y) -> V3 (-x) y (-0.5)
-
--- | Generate the face of a box
-
-genBoxFace :: BoxFace -> [V3 Float]
-genBoxFace boxFace = let face (min, max) = [ V2 min min, V2 max min, V2 min max, V2 min max, V2 max min, V2 max max ]
-                         vertices = map (faceFun boxFace) (face (-0.5, 0.5))
-                     in vertices
-
-
--- | A sample triangle
-
-triangleA :: Mesh
-triangleA = Mesh
-    { mAttributes   = Map.fromList
-        [ ("position",  A_V2F $ V.fromList [V2 1 1, V2 1 (-1), V2 (-1) (-1)])
-        , ("uv",        A_V2F $ V.fromList [V2 1 1, V2 0 1, V2 0 0])
-        ]
-    , mPrimitive    = P_Triangles
-    }
-
-
--- | A sample triangle
-
-triangleB :: Mesh
-triangleB = Mesh
-    { mAttributes   = Map.fromList
-        [ ("position",  A_V2F $ V.fromList [V2 1 1, V2 (-1) (-1), V2 (-1) 1])
-        , ("uv",        A_V2F $ V.fromList [V2 1 1, V2 0 0, V2 1 0])
-        ]
-    , mPrimitive    = P_Triangles
-    }
+    LC.renderFrame renderer
 
 
 -- | Convert a linear matrix to a lambacube one..
 
-convertMatrix :: Linear.M44 Float -> LC.M44F
-convertMatrix mat = let Linear.V4 (Linear.V4 a b c d)
-                                  (Linear.V4 e f g h)
-                                  (Linear.V4 i j k l)
-                                  (Linear.V4 m n o p) = mat
-                  in V4 (V4 a e i m)
-                        (V4 b f j n)
-                        (V4 c g k o)
-                        (V4 d h l p)
+convertMatrix :: M44 Float -> LC.M44F
+convertMatrix mat = let V4 (V4 a b c d)
+                           (V4 e f g h)
+                           (V4 i j k l)
+                           (V4 m n o p) = mat
+                    in LC.V4 (LC.V4 a e i m)
+                             (LC.V4 b f j n)
+                             (LC.V4 c g k o)
+                             (LC.V4 d h l p)
--- src/Game/Simulation.hs
+++ src/Game/Simulation.hs
@@ -12,15 +12,12 @@ module Game.Simulation
   )
 where
 
+import Linear
 import Framework
 import Game.Data
 import Game.Simulation.Input
 import Reactive.Banana
 import Reactive.Banana.Frameworks
-import Linear.Vector
-import Linear.V2
-import Linear.V3
-import Linear.Quaternion
 
 import qualified SDL.Event          as SDL
 import qualified SDL.Input.Keyboard as SDL
@@ -37,18 +34,21 @@ gameNetwork eInput eTick = do
   bTime <- stepper 0 eTime
 
   -- Camera position and orientation
-  bCamPos <- camPosition eInput eTick
   bCamRot <- camOrientation eInput
+  bCamPos <- camPosition eInput eTick bCamRot
 
   -- The overall game behavior
   return $ Game <$> bTime <*> bCamPos <*> bCamRot
 
 
 -- | A behavior describing the camera position
-camPosition :: InputEvent -> TickEvent -> MomentIO (Behavior (V3 Float))
-camPosition eInput eTick = do
+camPosition :: InputEvent
+            -> TickEvent
+            -> Behavior (Quaternion Float)
+            -> MomentIO (Behavior (V3 Float))
+camPosition eInput eTick bCamRot = do
   let speed = 1
-  let initialPos = (V3 0 0 (-50) :: V3 Float)
+  let initialPos = (V3 0 0 (50) :: V3 Float)
 
   bWDown <- keyDown eInput SDL.ScancodeW
   bSDown <- keyDown eInput SDL.ScancodeS
@@ -59,7 +59,7 @@ camPosition eInput eTick = do
 
   let eMouseMoved = mouseMoved eInput Absolute
 
-  let bVelocity = camVelocity speed <$> bWDown <*> bSDown <*> bADown <*> bDDown
+  let bVelocity = (camVelocity speed <$> bCamRot <*> bWDown <*> bSDown <*> bADown <*> bDDown)
 
   let bAddVelocity = (+) <$> bVelocity
   let eAddVelocity = bAddVelocity <@ eTick
@@ -72,15 +72,15 @@ camPosition eInput eTick = do
 -- | Calculates the camera velocity based on speed and inputs
 -- The (bool, bool, bool, bool) is (up, down, left, right)
 
-camVelocity :: Float -> Bool -> Bool -> Bool -> Bool -> V3 Float
-camVelocity speed up down left right =
+camVelocity :: Float -> Quaternion Float -> Bool -> Bool -> Bool -> Bool -> V3 Float
+camVelocity speed camRot up down left right =
   let forwardVel True False = -speed
       forwardVel False True =  speed
       forwardVel _     _    =  0
       horzVel    True False = -speed
       horzVel    False True =  speed
       horzVel    _     _    =  0
-  in V3 (horzVel left right) 0 (forwardVel up down)
+  in V3 (horzVel left right) 0 (forwardVel up down) *! fromQuaternion camRot
 
 
 -- | A behavior describing the camera orientation
